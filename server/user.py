37811c7c48ec85ce6e9396308763db74b6450409
zsuh3 <zionsuh3@gmail.com>
2024-10-23 22:33:43 +1300

    feat: create users json file if it doesn't exist

--- server/user.py
+++ server/user.py
@@ -1,7 +1,12 @@
 import json
+import os
 
 
 def load_users():
+    if not os.path.exists("../users.json"):
+        with open("../users.json", "w") as file:
+            json.dump({}, file)
+
     with open("../users.json", "r") as read_users:
         return json.load(read_users)
 
