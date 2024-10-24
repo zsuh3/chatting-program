11b0d72cbc93cd20b61703b273e9fe4f30eb3492
zsuh3 <zionsuh3@gmail.com>
2024-10-25 05:55:03 +1300

    feat: update chat ui

--- client/frontend/chat.py
+++ client/frontend/chat.py
@@ -1,12 +1,37 @@
+import threading
 import tkinter as tk
+from tkinter import scrolledtext
 
 
 class ChatScreen:
     def __init__(self, root, client_app):
         self.root = root
         self.client_app = client_app
-        self.root.title("Chat")
+        self.root.title("Chat Room")
         self.root.geometry("736x414")
         self.root.config(bg="#EDDFE0")
 
-        pass
+        # chat area
+        self.chat_area = scrolledtext.ScrolledText(self.root, wrap=tk.WORD, font=("Arial", 18), bg="#F5F5F7",
+                                                   fg="#705C53", highlightthickness=2, highlightbackground="#B7B7B7",
+                                                   state="disabled", height=10)
+        self.chat_area.pack(padx=20, pady=5, fill=tk.BOTH, expand=True)
+
+        # message area
+        self.message_input = tk.Entry(self.root, font=("Arial", 16), bg="#EDDFE0", fg="#705C53", relief="flat")
+        self.message_input.config(highlightbackground="#B7B7B7", highlightcolor="#B7B7B7")
+        self.message_input.pack(padx=20, pady=10, fill=tk.X)
+        self.message_input.bind("<Return>", self.send_message)
+
+        # send
+        self.send_button = tk.Button(self.root, text="Send", command=self.send_message, font=("Arial", 18),
+                                     bg="#705C53", fg="#F5F5F7", activebackground="#705C53", activeforeground="#F5F5F7",
+                                     borderwidth=0, highlightthickness=2, highlightbackground="#705C53", relief="flat")
+        self.send_button.pack(padx=20, pady=20, fill=tk.X)
+
+    def send_message(self, event=None):
+        message = self.message_input.get()
+        if message:
+            self.client_app.send_message(message)
+            self.append_message(f"You: {message}")
+            self.message_input.delete(0, tk.END)
